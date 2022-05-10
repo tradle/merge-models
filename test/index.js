a11d4e455600a0de88dee20713a8cf77baebe6fc
Martin Heidegger <martin.heidegger@gmail.com>
2022-05-10 18:28:47 +0900

    test: full coverage

--- test/index.js
+++ test/index.js
@@ -15,6 +15,21 @@ const B = {
   subClassOf: 'tradle.Form',
   properties: {}
 }
+const C = {
+  type: 'tradle.Model',
+  id: 'tradle.C',
+  title: 'C',
+  subClassOf: 'tradle.FinancialProduct',
+  forms: [],
+  properties: {}
+}
+const MyC = {
+  type: 'tradle.Model',
+  id: 'tradle.MyC',
+  title: 'MyC',
+  subClassOf: 'tradle.MyProduct',
+  properties: {}
+}
 
 test('basic', function (t) {
   const models = createManager()
@@ -22,6 +37,8 @@ test('basic', function (t) {
   t.same(models.get(), {})
   t.same(models.array(), [])
   t.same(models.subClassOf('tradle.Form'), {})
+  t.same(models.forms()[A.id], undefined)
+  t.same(models.products()[C.id], undefined)
   models.add([A])
 
   t.same(models.get(), {
@@ -41,6 +58,11 @@ test('basic', function (t) {
     title: 'A1'
   }
 
+  t.throws(() => models.add({
+    [A.id]: A,
+    foo: 'bar'
+  }), /already exists/)
+
   models.update([updatedA])
   t.same(models.get(), {
     [A.id]: updatedA
@@ -51,17 +73,25 @@ test('basic', function (t) {
     .add(DEFAULT_MODELS)
     .add([A])
     .add([B])
+    .add([C, MyC])
 
   t.same(models.rest(), {
     [A.id]: A,
-    [B.id]: B
+    [B.id]: B,
+    [C.id]: C,
+    [MyC.id]: MyC
   })
 
   t.same(models.subClassOf('tradle.Form')[B.id], B)
+  t.same(models.forms()[B.id], B)
+  t.same(models.subClassOf('tradle.FinancialProduct')[C.id], C)
+  t.same(models.products()[C.id], C)
 
   models.remove(A.id)
   t.same(models.rest(), {
-    [B.id]: B
+    [B.id]: B,
+    [C.id]: C,
+    [MyC.id]: MyC
   })
 
   models.reset()
